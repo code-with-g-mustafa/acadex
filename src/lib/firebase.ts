2210095f41c8ab391cc2e37d9d60da65bc07ac2c
GM Katbar <gm.katbar777@gmail.com>
2025-07-25 07:01:21 +0000

    GEMINI_API_KEY=AIzaSyCkspVMiCRMOW8RNbIcWfy_cMFYdMGEiJw
    NEXT_PUBLIC_FIRE

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAcG1bwdcTqjcKFjp0_vCrVR8SZfuMunc",
-  authDomain: "acadex-b7ce1.firebaseapp.com",
-  projectId: "acadex-b7ce1",
-  storageBucket: "acadex-b7ce1.appspot.com",
-  messagingSenderId: "907632257963",
-  appId: "1:907632257963:web:7326fb4c819c2b5d3b80ee",
-  measurementId: "G-TEJ9V1B2XZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
